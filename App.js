2390d53ebd4d17564263d09e1bd8b4c27939f184
Omar <omar.garciaf247@gmail.com>
2021-04-04 01:40:50 -0400

    adding search query

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
-import { Image, Text, TextInput, TouchableHighlight, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from "react-native";
 import { AppStyles } from "./Styles";
 import logo from "./assets/logo.png";
 import { useFonts } from "expo-font";
+import { searchImages } from "./unsplash.service";
 
 const App = () => {
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
   const [fontsLoaded] = useFonts({
     "DMSans-Medium": require("./assets/fonts/DMSans-Medium.ttf"),
     "DMSans-Regular": require("./assets/fonts/DMSans-Regular.ttf"),
   });
 
-  const search = () => {};
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
+  const search = () => {
+    if (searchQuery.trim() === "") {
+      Alert.alert("search cannot me empty");
+      return;
+    }
+
+    searchImages(searchQuery, page).then((res) => {
+      Alert.alert("no results were found with that query");
+      setImages(res.results);
+    });
+  };
 
   return (
     fontsLoaded && (
@@ -22,7 +45,6 @@ const App = () => {
             style={{
               display: "flex",
               flexDirection: "row",
-              flex: 1,
               alignItems: "flex-end",
             }}
           >
@@ -33,7 +55,7 @@ const App = () => {
             style={{
               display: "flex",
               flexDirection: "row",
-              marginTop: 8
+              marginTop: 8,
             }}
           >
             <TextInput
---
+++ unsplash.service.js
@@ -0,0 +1,7 @@
+const API_URL = "https://api.unsplash.com/";
+const API_TOKEN = "4iaJJNiV7jsWrxbFDeEC4OvWQKa9FfRnaO6I6gwO-nc";
+
+export const searchImages = (query, page) =>
+  fetch(
+    `${API_URL}/search/photos?client_id=${API_TOKEN}&query=${query}&page${page}`
+  ).then((res) => res.json());
