e086dec0de9dc1b1de525645f820ec04cee4d63d
Omar <omar.garciaf247@gmail.com>
2021-04-07 01:13:01 -0400

    moving keys to .env file

--- unsplash.service.js
+++ unsplash.service.js
@@ -1,9 +1,9 @@
+import { APP_UNSPLASH_API_TOKEN } from "@env";
 const API_URL = "https://api.unsplash.com/";
-const API_TOKEN = "4iaJJNiV7jsWrxbFDeEC4OvWQKa9FfRnaO6I6gwO-nc";
 
 export const searchImages = (query, page) =>
   fetch(
-    `${API_URL}/search/photos?client_id=${API_TOKEN}&query=${query.replace(
+    `${API_URL}/search/photos?client_id=${APP_UNSPLASH_API_TOKEN}&query=${query.replace(
       " ",
       "%20"
     )}&page=${page}`
